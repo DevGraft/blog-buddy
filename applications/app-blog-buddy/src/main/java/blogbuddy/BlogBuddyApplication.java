a9e26ace159b3b3b85ba05c5287cee31b971166c
DevGraft <devgraftteam@gmail.com>
2023-03-18 14:45:05 +0900

    close #2 멀티 프로젝트 구성 추가
    version
    - spring boot: 2.7.3
    - java: 17
    - gradle: 7.5
    
    directory
    - applications: 웹애플리케이션 관리
    - components: 웹애플리케이션 모듈 관리
    - features: 동작에 필요한 공통 관리

---
+++ applications/app-blog-buddy/src/main/java/blogbuddy/BlogBuddyApplication.java
@@ -0,0 +1,11 @@
+package blogbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBuddyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBuddyApplication.class, args);
+    }
+}
